c7f13853e7d36aa0c9cabf710e6da181006d6ff6
AllanCorp <135638821+allancorp23@users.noreply.github.com>
2024-03-15 20:27:50 +0200

    changes done...

--- dashboard.php
+++ dashboard.php
@@ -14,29 +14,9 @@ include("sessions.php");
 </head>
 <body>
     <div class="dashboard-container">
-        <div class="dashboard-left">
-
-            <div class="head">
-                <img src="./imgs/icon.ico" alt="">
-                <h1><a href="./dashboard.php">Saint Anne School</a></h1>
-                <div class="line"></div>
-            </div>
-
-            <div class="links">
-                <a href="./dashboard.php"><img src="./imgs/dashboard.ico" alt=""> Dashboard</a>
-                <a href="./products.php"><img src="./imgs/products.ico" alt=""> Products</a>
-                <a href="./stock_in.php"><img src="./imgs/stockin.ico" alt=""> Stock In</a>
-                <a href="./stock_out.php"><img src="./imgs/stockout.ico" alt=""> Stock Out</a>
-                <a href="./report.php"><img src="./imgs/report.ico" alt=""> Report</a>
-                <a href="./account_settings"><img src="./imgs/settings.ico" alt=""> Account Settings</a>
-            </div>
-
-            <div class="end">
-                <p><?php echo $_SESSION['school_user'];?></p>
-                <a href="./logout.php">Logout</a>
-            </div>
-
-        </div>
+        <?php
+            include("sidebar.php");
+        ?>
 
         <div class="dashboard-right">
 
